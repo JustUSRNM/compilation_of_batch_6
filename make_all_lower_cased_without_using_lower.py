277807ca8e6d5a40b399b78e55464bd17673489e
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-27 18:27:46 +0800

    finished print in lower case program

--- make_all_lower_cased_without_using_lower.py
+++ make_all_lower_cased_without_using_lower.py
@@ -1,4 +1,6 @@
 # input a word
-word= input('Please enter a word in incorrect casing')
+word= input('Please enter a word in incorrect casing: ')
 # using casefold() to make all lower cased
+lower_cased= word.casefold()
 # print the word in lower case
+print (lower_cased)
