0f94d58f03d9aa3f396c0f6df77c42de96fb50cd
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-27 18:12:45 +0800

    finished the remove prefix program

--- remove_prefix_without_using_removeprefix.py
+++ remove_prefix_without_using_removeprefix.py
@@ -1,6 +1,9 @@
 # input a word and then the prefix
-word= input('Please enter a word')
-parameter= input('Please enter a prefix that is in the word')
+word= input('Please enter a word: ')
+parameter= input('Please enter a prefix that is in the word: ')
 # check if the prefix is in the inputted word
+if word.startswith(parameter):
 # get the len of prefix and remove it from the word
+    no_prefix= word[len(parameter):]
 # print the word without the prefix
+    print (no_prefix)
