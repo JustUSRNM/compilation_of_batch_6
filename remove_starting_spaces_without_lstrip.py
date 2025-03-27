02619d2e915dbf89cbda0f7fb9f4f326ad21bb32
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-27 18:08:39 +0800

    finished the remove starting spaces program

--- remove_starting_spaces_without_lstrip.py
+++ remove_starting_spaces_without_lstrip.py
@@ -1,5 +1,10 @@
 # input a word with starting spaces
-word= input('Please enter a word with mutiple starting spaces')
+word= input('Please enter a word with mutiple starting spaces:')
 # count the number of starting spaces
+number_of_spaces= 0
+while word[number_of_spaces] == " ":
+    number_of_spaces += 1
 # remove the starting spaces
+word_no_spaces= word[number_of_spaces:]
 # print inputted word without the starting spaces
+print (word_no_spaces)
