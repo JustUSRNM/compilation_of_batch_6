1017eab0981100164a1de5156e370f562d85f8e0
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-27 18:32:34 +0800

    finished the isupper program

--- check_if_upper_without_isupper.py
+++ check_if_upper_without_isupper.py
@@ -1,4 +1,10 @@
 # input a word
-word= input('Please enter a word')
+word= input('Please enter a word: ')
 # using for loop, check if all is upper cased then break when lower case is found
+is_upper= True
+for char in word:
+    if 'a' <= char <= 'z':
+        is_upper= False
+        break
 # print the boolean
+print (is_upper)
