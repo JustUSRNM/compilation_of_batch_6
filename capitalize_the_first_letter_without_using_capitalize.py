701f4fe0cbf2fbc32219864013f9c1f2823ec505
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-28 11:05:44 +0800

    finished the capitalize program

--- capitalize_the_first_letter_without_using_capitalize.py
+++ capitalize_the_first_letter_without_using_capitalize.py
@@ -1,4 +1,6 @@
 # input a word
 word= input('Please enter a word: ')
 # using index, make the first letter capital the rest small
+capitlaize= word[0].upper() + word[1:].lower()
 # print the word in capitalize
+print (capitlaize)
