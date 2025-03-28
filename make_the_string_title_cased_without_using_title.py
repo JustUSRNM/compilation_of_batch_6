a12ba11dd2a06a9ec1e68d4ad2f7489db532667b
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-28 11:12:06 +0800

    finished the title case program; finished batch 6

--- make_the_string_title_cased_without_using_title.py
+++ make_the_string_title_cased_without_using_title.py
@@ -1,5 +1,10 @@
 # input a text
 text= input('Please enter a text: ')
 # split the text into words
+words= text.split()
+titled_words=[]
 # in each word use .upper at the first letter and .lower to the rest
+for individual_word in words:
+    titled_words.append(individual_word[0].upper() + individual_word[1:])
 # print the recombined words to make the text in title case
+print (" ".join(titled_words))
