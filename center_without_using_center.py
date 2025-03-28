368c59c42c33ff6732a6effbfb183dec7a72552b
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-28 10:58:28 +0800

    finished the center the word program

--- center_without_using_center.py
+++ center_without_using_center.py
@@ -2,4 +2,8 @@
 word= input('Please enter a word: ')
 parameter= int(input('Enter how much spaces do you want the word be centered in: '))
 # using arithmetic to get the how many padding is need on both sides
+total_spaces = parameter - len(word)
+left_spaces = total_spaces // 2
+right_spaces = total_spaces - left_spaces
 # print the word centered by the parameter
+print (" "*left_spaces + word + " "*right_spaces)
