825463c0282e91c9cfef65ccd8277746fa053af9
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-28 10:53:09 +0800

    finished the left justify program

--- left_justify_without_using_ljust.py
+++ left_justify_without_using_ljust.py
@@ -2,4 +2,9 @@
 word= input('Please enter a word: ')
 parameter= int(input('Enter what width the word be in: '))
 # width subtracted to the length of the word, is how many spaces are added
+if parameter > len(word):
+    spaces= parameter - len(word)
+elif parameter <= len(word):
+    spaces = 0
 # print the word in the given width
+print (word+ spaces*" ")
