6524e2f04cfc5078a07331f74c1985317121816c
Rafael Almanza <almanzajuanrafael@gmail.com>
2025-03-28 11:02:18 +0800

    finished the swap casing program

--- swap_the_casing_without_using_swapcase.py
+++ swap_the_casing_without_using_swapcase.py
@@ -1,4 +1,13 @@
 # input a word
 word= input('Please enter a word: ')
 # using for loop, check each character then switch they're casing with upper and lower
+swapped_word=""
+for char in word:
+    if 'a' <= char <= 'z':
+        swapped_word += char.upper()
+    elif 'A' <= char <= 'Z':
+        swapped_word += char.lower()
+    else:
+        swapped_word += char
 # print the word with it's casing swapped
+print (swapped_word)
